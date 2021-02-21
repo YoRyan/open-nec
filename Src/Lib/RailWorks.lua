da5e5bcf1fca4730a05f5b6b29f1f489a00914f9
Ryan Young <ryan@youngryan.com>
2021-02-20 19:52:24 -0800

    Change messages to info boxes to fit long strings.

--- Src/Lib/RailWorks.lua
+++ Src/Lib/RailWorks.lua
@@ -4,7 +4,7 @@ RailWorks = {}
 RailWorks.__index = RailWorks
 
 function RailWorks.showmessage(msg)
-  SysCall("ScenarioManager:ShowMessage", tostring(msg), "", 1)
+  SysCall("ScenarioManager:ShowMessage", tostring(msg), "", 2)
 end
 
 -- Run the provided function and arguments with pcall and report any errors
