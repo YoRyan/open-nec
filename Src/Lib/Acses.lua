28ede0b215787c551f8fe9ef42995166e8904584
Ryan Young <ryan@youngryan.com>
2021-02-24 19:59:32 -0800

    ACSES: fix style

--- Src/Lib/Acses.lua
+++ Src/Lib/Acses.lua
@@ -91,9 +91,9 @@ function Acses._printlimits(self)
     return res
   end
   self._sched:print("Track: " .. fspeed(self.config.gettrackspeed_mps()) .. " "
-      .. "Sensed: " .. fspeed(self.trackspeed.state.speedlimit_mps) .. "\n"
-      .. "Forward: " .. dump(self.config.getforwardspeedlimits())
-      .. "Backward: " .. dump(self.config.getbackwardspeedlimits()))
+    .. "Sensed: " .. fspeed(self.trackspeed.state.speedlimit_mps) .. "\n"
+    .. "Forward: " .. dump(self.config.getforwardspeedlimits())
+    .. "Backward: " .. dump(self.config.getbackwardspeedlimits()))
 end
 
 function Acses._doenforce(self)
