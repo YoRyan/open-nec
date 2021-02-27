b172d43289ae44b0aec2060fb7fd3c0ceb179c08
Ryan Young <ryan@youngryan.com>
2021-02-27 01:16:35 -0800

    AEM-7: Shadow the conflicting sim scripts in Fan Railer's mod.

--- Release.bat
+++ Release.bat
@@ -1,9 +1,15 @@
 rem Build a distributable ZIP archive for a release.
 
 xcopy Mod Release\Assets\ /s /y
+
 rem Ensure compatibility with Fan Railer's AEM-7 overhaul mod.
 copy Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Engine\RailVehicle_EngineScript.out^
     Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Engine\EngineScript.out /y
+mkdir "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\AC Physics\"
+copy NUL "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\AC Physics\AEM-7 SimScript.out"
+mkdir "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\DC Physics\"
+copy NUL "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\DC Physics\AEM-7 SimScript.out"
+
 del OpenNEC.zip
 cd Release\
 7z a ..\OpenNEC.zip .
