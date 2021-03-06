98389291cb29639c31267a6b7f34156a2c2694e7
Ryan Young <ryan@youngryan.com>
2021-03-06 01:02:32 -0800

    Delete unused Tables.find function.

--- Src/Lib/Tables.lua
+++ Src/Lib/Tables.lua
@@ -3,18 +3,6 @@
 Tables = {}
 Tables.__index = Tables
 
--- Apply fn to all values in a numeric-indexed table and return the index of the
--- first value for which fn evaluates to true. If no such value exists, return nil.
-function Tables.find(t, fn)
-  return table.foreachi(t, function (i, v)
-    if fn(v) then
-      return i
-    else
-      return nil
-    end
-  end)
-end
-
 -- Copy the provided iterator expression list into a table.
 function Tables.fromiterator(...)
   local t = {}
