10d086e4fbb0bc3f23c7233f0532904e0c426381
Ryan Young <ryan@youngryan.com>
2021-03-10 11:45:25 -0800

    AEM-7: move Fan Railer fixes out of release script

--- Release.bat
+++ Release.bat
@@ -3,14 +3,6 @@ rem Build a distributable ZIP archive for a release.
 xcopy Mod Release\Assets\ /s /y
 copy Readme.md Release\Readme.md
 
-rem Ensure compatibility with Fan Railer's AEM-7 overhaul mod.
-copy Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Engine\RailVehicle_EngineScript.out^
-    Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Engine\EngineScript.out /y
-mkdir "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\AC Physics\"
-copy NUL "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\AC Physics\AEM-7 SimScript.out"
-mkdir "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\DC Physics\"
-copy NUL "Release\Assets\RSC\NorthEastCorridor\RailVehicles\Electric\AEM7\Default\Simulation\DC Physics\AEM-7 SimScript.out"
-
 del Mod.zip
 cd Release\
 7z a ..\Mod.zip .
---
+++ Src/Mod/RSC/NorthEastCorridor/RailVehicles/Electric/AEM7/Default/Engine/EngineScript.lua
@@ -0,0 +1,1 @@
+./RailVehicle_EngineScript.lua
