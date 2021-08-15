7493ddc839983563e58d1ba0edbf4383372c529f
Ryan Young <ryan@youngryan.com>
2021-08-15 00:37:24 -0700

    fix(MP36): make suppression threshold more lenient

--- Src/Mod/DTG/WashingtonBaltimore/RailVehicles/CommonScripts/MP36PH_Engine_Script.lua
+++ Src/Mod/DTG/WashingtonBaltimore/RailVehicles/CommonScripts/MP36PH_Engine_Script.lua
@@ -48,7 +48,7 @@ Initialise = Misc.wraperrors(function()
     getacceleration_mps2 = function() return state.acceleration_mps2 end,
     getacknowledge = function() return state.acknowledge end,
     doalert = function() adu:doatcalert() end,
-    getbrakesuppression = function() return state.train_brake >= 0.75 end
+    getbrakesuppression = function() return state.train_brake >= 0.6 end
   }
 
   acses = Acses:new{
