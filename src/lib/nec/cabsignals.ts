e4d9817924cd857ed444f9e492ac4c0a6cfc8934
Ryan Young <ryan@youngryan.com>
2023-11-13 19:58:08 +0000

    refactor: use switch-case to parse signal messages

--- src/lib/nec/cabsignals.ts
+++ src/lib/nec/cabsignals.ts
@@ -297,63 +297,79 @@ export const njTransitAtc: AtcSystem<NjTransitAspect> = {
  */
 export function toPulseCode(signalMessage: string) {
     // Signals scripted by Brandon Phelan.
-    const [, , sig, speed] = string.find(signalMessage, "^sig(%d)speed(%d+)$");
-    if (sig === "1" && speed == "150") {
-        return PulseCode.C_180_180;
-    } else if (sig === "1" && speed == "100") {
-        return PulseCode.C_270_270;
-    } else if (sig === "1") {
-        return PulseCode.C_180_0;
-    } else if (sig === "2") {
-        return PulseCode.C_120_120;
-    } else if (sig === "3") {
-        return PulseCode.C_270_0;
-    } else if (sig === "4") {
-        return PulseCode.C_120_0;
-    } else if (sig === "5") {
-        return PulseCode.C_75_75;
-    } else if (sig === "6") {
-        return PulseCode.C_75_0;
-    } else if (sig === "7" && speed === "60") {
-        return PulseCode.C_420_0;
-    } else if (sig === "7") {
-        return PulseCode.C_0_0;
-    }
-    const [, , stop] = string.find(signalMessage, "^sig7stop(%d+)$");
-    if (stop !== undefined) {
-        return PulseCode.C_0_0;
+    {
+        const [, , sig, speed] = string.find(signalMessage, "^sig(%d)speed(%d+)$");
+        switch (sig) {
+            case "1":
+                switch (speed) {
+                    case "150":
+                        return PulseCode.C_180_180;
+                    case "100":
+                        return PulseCode.C_270_270;
+                    default:
+                        return PulseCode.C_180_0;
+                }
+            case "2":
+                return PulseCode.C_120_120;
+            case "3":
+                return PulseCode.C_270_0;
+            case "4":
+                return PulseCode.C_120_0;
+            case "5":
+                return PulseCode.C_75_75;
+            case "6":
+                return PulseCode.C_75_0;
+            case "7":
+                switch (speed) {
+                    case "60":
+                        return PulseCode.C_420_0;
+                    default:
+                        return PulseCode.C_0_0;
+                }
+        }
+
+        const [, , stop] = string.find(signalMessage, "^sig7stop(%d+)$");
+        if (stop !== undefined) {
+            return PulseCode.C_0_0;
+        }
     }
 
     // Signals scripted by DTG for Amtrak and NJ Transit DLC's.
-    const [, , sig2] = string.find(signalMessage, "^sig(%d+)");
-    if (sig2 === "1") {
-        return PulseCode.C_180_0;
-    } else if (sig2 === "2") {
-        return PulseCode.C_120_120;
-    } else if (sig2 === "3") {
-        return PulseCode.C_270_0;
-    } else if (sig2 === "4") {
-        return PulseCode.C_120_0;
-    } else if (sig2 === "5") {
-        return PulseCode.C_75_75;
-    } else if (sig2 === "6") {
-        return PulseCode.C_75_0;
-    } else if (sig2 === "7") {
-        return PulseCode.C_0_0;
+    {
+        const [, , sig] = string.find(signalMessage, "^sig(%d+)");
+        switch (sig) {
+            case "1":
+                return PulseCode.C_180_0;
+            case "2":
+                return PulseCode.C_120_120;
+            case "3":
+                return PulseCode.C_270_0;
+            case "4":
+                return PulseCode.C_120_0;
+            case "5":
+                return PulseCode.C_75_75;
+            case "6":
+                return PulseCode.C_75_0;
+            case "7":
+                return PulseCode.C_0_0;
+        }
     }
 
     // Signals scripted by DTG for Metro-North DLC's.
-    const [, , code] = string.find(signalMessage, "^[MN](%d%d)");
-    if (code === "10") {
-        return PulseCode.C_180_0;
-    } else if (code === "11") {
-        return PulseCode.C_120_0;
-    } else if (code === "12") {
-        return PulseCode.C_75_0;
-    } else if (code === "13" || code === "14") {
-        return PulseCode.C_0_0;
-    } else if (code === "15") {
-        return PulseCode.C_0_0;
+    {
+        const [, , code] = string.find(signalMessage, "^[MN](%d%d)");
+        switch (code) {
+            case "10":
+                return PulseCode.C_180_0;
+            case "11":
+                return PulseCode.C_120_0;
+            case "12":
+                return PulseCode.C_75_0;
+            case "13":
+            case "14":
+            case "15":
+                return PulseCode.C_0_0;
+        }
     }
 
     return undefined;
